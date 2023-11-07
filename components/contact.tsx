813a3d1ed0442728eee36868881d3731745190cd
tkamit6 <76872254+tkamit6@users.noreply.github.com>
2023-11-07 13:14:06 +0530

    UP

--- components/contact.tsx
+++ components/contact.tsx
@@ -5,24 +5,30 @@ import SectionHeading from './section-heading'
 import { FaPaperPlane } from 'react-icons/fa';
 import { delay, motion } from 'framer-motion';
 import { sendEmail } from '@/action/senEmail';
-import axios from 'axios'
+import toast from "react-hot-toast";
 
 export default function Contact() {
-    const [formData, setFormData] = useState<{ senderMessage: string; senderEmail: string }>({
-        senderEmail: '',
-        senderMessage: '',
-    })
+    // const [formData, setFormData] = useState<{ senderMessage: string; senderEmail: string }>({
+    //     senderEmail: '',
+    //     senderMessage: '',
+    // })
 
-   
-    const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    const [senderEmail, setSenderEmail] = useState('')
+    const [message, setMessage] = useState('');
+
+
+    const sendMessage = async (e: React.FormEvent) => {
         e.preventDefault()
-        try {
-            await axios.post("/component/api/contact", formData)
-                .then(response => { console.log(response) });
-        } catch (error) {
-            console.log(error);
-
-        }
+        toast.success("Email sent successfully!");
+        const res = await fetch('/api/contact', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ senderEmail, message }),
+        });
+        const data = await res.json();
+        console.log(data);
     }
 
 
@@ -32,10 +38,10 @@ export default function Contact() {
             <p className='text-gray-700 text-center'>Please contact me directly at <a className='underline' href='mailto:tkamit6@gmail.com'>tkamit6@gmail.com</a>{" "}
                 or through this form <br /> +91-8802665778</p>
             <form onSubmit={sendMessage} className='mt-10 flex flex-col gap-6'>
-                <input name="senderEmail"
-                    value={formData.senderEmail} type="email" required placeholder='Your Email' className='h-12 rounded-lg border border-black/10 p-4 shadow-sm dark:bg-gray-50/10' />
-                <textarea name="senderMessage"
-                    value={formData.senderMessage} required placeholder='Your Message' className='h-52 rounded-lg border-black/10 p-4 shadow-md dark:bg-gray-50/10' />
+                <input name="senderEmail" onChange={(e) => setSenderEmail(e.target.value)} value={senderEmail}
+                    type="email" required placeholder='Your Email' className='h-12 rounded-lg border border-black/10 p-4 shadow-sm dark:bg-gray-50/10' />
+                <textarea name="senderMessage" onChange={(e) => setMessage(e.target.value)} value={message}
+                    required placeholder='Your Message' className='h-52 rounded-lg border-black/10 p-4 shadow-md dark:bg-gray-50/10' />
                 <button type='submit' className='group hover:scale-110 h-[3rem] w-[8rem] bg-gray-900  text-white rounded-full outline-none transition justify-center items-center flex gap-2'>Submit <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:opacity-100 group-hover:translate-x-2' /> </button>
             </form>
         </motion.section>
