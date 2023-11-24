ef51c76e26f53912855294840f7f933f334fa2f1
tkamit6 <76872254+tkamit6@users.noreply.github.com>
2023-11-24 14:16:53 +0530

    nn

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='!scroll-smooth'>
+      <meta name="google-site-verification" content="tcLcVD1vFzsT_f9GRWgA4Lb8A5_4nYpwb81hRZ5YyvE" />
       <body className={`${inter.className} bg-gray-50 text-gray-950 pt-28 sm:pt-36 relative !scroll-smooth dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
         <div className=' absolute top-[-6rem] right-[6rem] h-[31.25rem] w-[32.24rem] -z-10 rounded-full blur-[6rem] sm:w-[68rem]'></div>
         <div className='bg-[#bdb7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50.24rem] -z-10 rounded-full blur-[10rem] sm:w-[68rem] md:left-[-35rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left[-5rem]'></div>
